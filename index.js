da391485286bb8e56f482e72f71735aada261781
Michael Paukov <michael.paukov@gmail.com>
2023-01-05 16:57:26 +0100

    fix43

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const { DB_HOST, PORT = 4000 } = process.env;
+const { DB_HOST, PORT = 6000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
