958297a230ec74f9f9ae8a42ea5a0f0138d93d91
Michael Paukov <michael.paukov@gmail.com>
2023-01-06 10:58:07 +0100

    remove app

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
-const app = require('./app');
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./api-doc.json');
+require('dotenv').config();
+
+const { authRouter, usersRouter, transactionsRouter } = require('./routes/api');
+const { errorHandler } = require('./middlewares');
+
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/transactions', transactionsRouter);
+app.use(errorHandler);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const { DB_HOST, PORT = 6000 } = process.env;
 
